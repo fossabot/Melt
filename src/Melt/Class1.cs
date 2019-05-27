d87f3eebe642f0641d4d8f38dc788b33881ab678
0x0001F36D <viyrex.aka.yuyu@gmail.com>
2019-05-27 17:21:54 +0800

    加入專案檔案。

---
+++ src/Melt/Class1.cs
@@ -0,0 +1,35 @@
+﻿
+namespace Melt
+{
+    using System;
+
+    public abstract class ConverterBase
+    {
+        protected abstract Type Consult { get; }
+
+        public virtual bool TypeMatch(Type type)
+        {
+            return this.Consult.FullName == type.FullName;
+        }
+
+        protected abstract object OnConvertFromBytes(byte[] bytes, IConverterPool pool);
+
+        protected abstract byte[] OnConvertToBytes(object graph, IConverterPool pool);
+
+
+        public byte[] ToBytes(object graph)
+        {
+
+        }
+
+        public object FromBytes(byte[] bytes)
+        {
+
+        }
+
+    }
+    public interface IConverterPool
+    {
+
+    }
+}
---
+++ test/Melt.UnitTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Melt.UnitTest
+{
+    [TestClass]
+    public class PrimitiveTypeTest
+    {
+        private const string PRIMITIVE_TYPE_STRING = "Primitive Type";
+
+        [TestMethod]
+        [TestCategory(PRIMITIVE_TYPE_STRING)]
+        public void BoolToBytesTest()
+        {
+        }
+    }
+}
